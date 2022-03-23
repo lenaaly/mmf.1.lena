eecc1d8ecd926b952db307391b3fcb04eef4c0a8
Lena Aly <101528160+lenaaly@users.noreply.github.com>
2022-03-23 23:03:05 +0000

    base component

---
+++ mmf.py
@@ -0,0 +1,19 @@
+# functions go here
+
+#''''''''''''' Main Routine ''''''''''''
+
+# set up dictionaries / lists needed to hold data
+# Ask user if they have used the program before and show instructions if nessesary
+#loop to get ticket details
+
+# get name (cant be blank)
+# get age (between 12 and 130)
+# calulate ticket price
+# loop to ask for snacks
+# calculate snack price
+
+# ask for payment method (amd apply surcharge if necessary)
+
+#calculate total sales and profit
+
+#output data to text file
