462ed5b227b19f43160c2fe5cfae510cd59d6b5d
Lena Aly <101528160+lenaaly@users.noreply.github.com>
2022-03-25 03:55:11 +0000

    coded number checker

--- mmf_base.py
+++ mmf_base.py
@@ -22,12 +22,38 @@ def not_blank(question, error_message):
 
 # set up dictionaries / lists needed to hold data
 # Ask user if they have used the program before and show instructions if nessesary
+
+
 #loop to get ticket details
 
+# initialise loop so that it runs at least once
+
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+  print("You have {} seats " "left".format(MAX_TICKETS -count))
+
+  # get details...
+ 
 # get name (cant be blank)
-name = not_blank("Name: ", 
+  # get name (cant be blank)
+  name = not_blank("Name: ", 
                  "Sorry - this cant be blank,"
                 "please enter your name")
+
+  count += 1
+  print()
+
+# tells user how many seats are left
+if count == MAX_TICKETS:
+  print("You have sold all the available tickets!")
+
+# warns user that only one seat is left!
+else:
+  print("You have sold {} tickets.  \n"
+       "There are {} places still available".format(count, MAX_TICKETS - count))
 
 # get age (between 12 and 130)
 # calulate ticket price
--- v2ticket_loop.py
+++ v2ticket_loop.py
@@ -14,9 +14,11 @@ while name != "xxx" and count < MAX_TICKETS:
   count += 1
   print()
 
+# tells user how many seats are left
 if count == MAX_TICKETS:
   print("You have sold all the available tickets!")
 
+# warns user that only one seat is left!
 else:
   print("You have sold {} tickets.  \n"
        "There are {} places still available".format(count, MAX_TICKETS - count))
