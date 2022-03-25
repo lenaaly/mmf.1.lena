0647175d003bbea330c28f0a7cf19eea6f83881e
Lena Aly <101528160+lenaaly@users.noreply.github.com>
2022-03-25 00:01:06 +0000

    Updated loop code

---
+++ ticket_loop.py
@@ -0,0 +1,15 @@
+# start of loop
+
+# initialise loop so that it runs at least once
+
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+
+  # get details...
+  name = input("Name: ")
+  count += 1
+
+  
