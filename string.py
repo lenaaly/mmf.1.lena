a72ebc2166f52b301055d4a9651fd024a1791c80
Lena Aly <101528160+lenaaly@users.noreply.github.com>
2022-03-29 22:38:37 +0000

    added string

--- string.py
+++ string.py
@@ -1,28 +1,3 @@
-def yes_no(question):
-
-  error = "Please answer yes / no"
-
-  valid = False
-  while not valid:
-
-    # ask question and put response in lowercase
-    response = input(question).lower()
-
-    if response == "yes" or response == "y":
-      return "yes"
-    elif response == "no" or response == "n":
-      return "no"
-    else:
-      print(error)
-
-# Main routine goes here
-
-for item in range(0, 6):
-  want_snacks = yes_no("Do you want snacks? ")
-  print("Answer OK, you said:", want_snacks)
-  print()
-
-
 # string checking functions, takes in
 # question and list of valid responses
 def string_checker(question, to_check):
