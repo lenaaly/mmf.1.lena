350a7c9b8431bffd807c5602bbb4be6952494123
Lena Aly <101528160+lenaaly@users.noreply.github.com>
2022-05-18 09:44:46 +0000

    v4 base and snack lists

---
+++ v2snack_list.py
@@ -0,0 +1,23 @@
+# initialise snack lists...
+
+popcorn = []
+mms = []
+pita_chips = []
+water = []
+orange_juice = []
+
+snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
+
+snack_menu_dict = {
+  'Popcorn': popcorn,
+  'Water': water,
+  'Pita Chips': pita_chips,
+  'M&Ms': mms,
+  'Orange Juice': orange_juice
+}
+
+test_data = {
+  [[1, 'Water'], [1, 'Pita Chips'], [1, 'M&Ms']],
+  [[1, 'Pita Chips'], [1, 'Orange Jucie'], [2, 'M&Ms']],
+  [[1, 'Water']]
+}
