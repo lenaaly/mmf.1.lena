5b07d07d14f06cf0d6727658f31b5d206d711708
Lena Aly <101528160+lenaaly@users.noreply.github.com>
2022-05-18 10:26:11 +0000

    v4 base editing

--- v3snack_list.py
+++ v3snack_list.py
@@ -1,7 +1,10 @@
+#hard coded way to test  pandas methods
+import pandas
 
 # Initialise snack lists...
 
-names = ['Sarah', 'Ayah' 'Kate', 'Hannah', 'Ella']
+
+all_names = ['Sarah', 'Ayah', 'Kate', 'Hannah', 'Ella']
 
 popcorn = []
 mms = []
@@ -11,13 +14,17 @@ orange_juice = []
 
 snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 
-snack_menu_dict = {
-    'Popcorn': popcorn,
-    'Water': water,
-    'Pita Chips': pita_chips,
-    'M&Ms': mms,
-    'Orange Juice': orange_juice
-    }
+# data drane dictionary 
+movie_data_dict = {
+  'Name': all_names,
+  'Popcorn': popcorn,
+  'Water': water,
+  'Pita Chips': pita_chips,
+  'M&Ms': mms,
+  'Orange Juice': orange_juice
+
+}
+
 
 test_data = [
     [[2, 'Popcorn'], [1, 'Pita Chips'], [1, 'Orange Juice']],
@@ -44,12 +51,20 @@ for client_order in test_data:
         if len(item) > 0:
             to_find = (item[1])
             amount = (item[0])
-            add_list = snack_menu_dict[to_find]
+            add_list = movie_data_dict[to_find]
             add_list[-1] = amount
 
+
 print()
+print("Names:", all_names)
 print("Popcorn: ", snack_lists[0])
 print("M&Ms:", snack_lists[1])
 print("Pita Chips:", snack_lists[2])
 print("Water:", snack_lists[3])
 print("Orange Juice:", snack_lists[4])
+print()
+
+# print details...
+movie_frame = pandas.DataFrame(movie_data_dict)
+movie_frame = movie_frame.set_index('Name')
+print(movie_frame)
