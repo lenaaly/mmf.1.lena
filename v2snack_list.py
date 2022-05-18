913c2a854df73ce422b0446be7cd3affa8f91c9c
Lena Aly <101528160+lenaaly@users.noreply.github.com>
2022-05-18 09:54:12 +0000

    v2 snack list

--- v2snack_list.py
+++ v2snack_list.py
@@ -1,4 +1,9 @@
-# initialise snack lists...
+#hard coded way to test  pandas methods
+import pandas
+
+# Initialise snack lists...
+
+names = ['Sarah', 'Ayah' 'Kate', 'Hannah', 'Ella']
 
 popcorn = []
 mms = []
@@ -9,15 +14,44 @@ orange_juice = []
 snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 
 snack_menu_dict = {
-  'Popcorn': popcorn,
-  'Water': water,
-  'Pita Chips': pita_chips,
-  'M&Ms': mms,
-  'Orange Juice': orange_juice
-}
+    'Popcorn': popcorn,
+    'Water': water,
+    'Pita Chips': pita_chips,
+    'M&Ms': mms,
+    'Orange Juice': orange_juice
+    }
 
-test_data = {
-  [[1, 'Water'], [1, 'Pita Chips'], [1, 'M&Ms']],
-  [[1, 'Pita Chips'], [1, 'Orange Jucie'], [2, 'M&Ms']],
-  [[1, 'Water']]
-}
+test_data = [
+    [[2, 'Popcorn'], [1, 'Pita Chips'], [1, 'Orange Juice']],
+    [[]],
+    [[1, 'Water']],
+    [[1, 'Popcorn'], [1, 'Orange Juice']],
+    [[1, 'M&Ms'], [1, 'Pita Chips'], [3, 'Orange Juice']]
+]
+
+count = 0
+for client_order in test_data:
+
+    # Assume no snacks have been bought...
+    for item in snack_lists:
+        item.append(0)
+
+    # print(snack_lists)
+
+    # get order (hard coded for easy testing)...
+    snack_order = test_data[count]
+    count += 1
+
+    for item in snack_order:
+        if len(item) > 0:
+            to_find = (item[1])
+            amount = (item[0])
+            add_list = snack_menu_dict[to_find]
+            add_list[-1] = amount
+
+print()
+print("Popcorn: ", snack_lists[0])
+print("M&Ms:", snack_lists[1])
+print("Pita Chips:", snack_lists[2])
+print("Water:", snack_lists[3])
+print("Orange Juice:", snack_lists[4])
